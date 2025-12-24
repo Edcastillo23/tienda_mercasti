0f29e1fbb6794e8e030ec0502d953e5540863029
Edinson Castillo <castillo192011@gmail.com>
2025-12-24 17:22:20 -0500

    Se crea la base de datos y se alimenta la pagina catalogo desde esta

--- tienda/catalogo/models.py
+++ tienda/catalogo/models.py
@@ -1,3 +1,21 @@
 from django.db import models
 
 # Create your models here.
+class Producto(models.Model):
+    # 'id' se crea automáticamente por Django como un AutoField (Clave Primaria)
+    
+    nombre = models.CharField(max_length=100)
+    precio = models.IntegerField()  
+    descripcion = models.TextField()
+    
+    # En base de datos se guarda la ruta como texto
+    imagen = models.CharField(max_length=200, default='imagengenerica.jpg')
+    
+    stock = models.IntegerField(default=0)
+    oferta = models.BooleanField(default=False)
+    
+    # Recomendación extra: añade la categoría para poder filtrar
+    #categoria = models.CharField(max_length=50, null=True, blank=True)
+
+    def __str__(self):
+        return self.nombre
