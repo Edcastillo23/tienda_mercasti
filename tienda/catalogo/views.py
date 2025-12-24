9f8ee867cf8a0b99c30301adee369729e6f2dea0
Edinson Castillo <castillo192011@gmail.com>
2025-12-24 12:14:33 -0500

    Se mejora el footer y la barra de navegación queda fija

--- tienda/catalogo/views.py
+++ tienda/catalogo/views.py
@@ -1,3 +1,20 @@
 from django.shortcuts import render
 
 # Create your views here.
+def lista_productos(request):
+    lista_productos = [
+        {
+            'Remache laton': '10-10C', 
+            'precio': 124_986
+        },
+        {
+            'Pasador muelle Trasero Doble Cuñero': '1-3/8 7"', 
+            'precio': 72_000
+        },
+        
+        ]
+    
+
+
+
+    
