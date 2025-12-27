1d84460ff1c7dae53d53f1980532618304bd99fa
Edinson Castillo <castillo192011@gmail.com>
2025-12-26 19:33:06 -0500

    Se agrega paginación y se enlaza la pagina catalogo a la opción del  menú llamada tienda

--- tienda/catalogo/views.py
+++ tienda/catalogo/views.py
@@ -1,12 +1,17 @@
 from django.shortcuts import render
+from django.core.paginator import Paginator
 from catalogo.models import Producto
 
 # Create your views here.
 def lista_productos(request):
-    productos = Producto.objects.all()
-    
-    context_catalogo = {'productos': productos}
+    productos = Producto.objects.all().order_by('id')
+
+    paginator = Paginator(productos, 2)  # Mostrar 4 productos por página
+    page_number = request.GET.get('page')
+
+    page_obj = paginator.get_page(page_number)
+
+    context_catalogo = {'productos': page_obj}
     return render(request, 'catalogo/lista_productos.html', context_catalogo)
         
-
 
