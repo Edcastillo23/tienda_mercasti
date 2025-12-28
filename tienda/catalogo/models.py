ee6e2134a3472392f32748df354847200dcbbd41
Edinson Castillo <castillo192011@gmail.com>
2025-12-28 17:33:02 -0500

    actualizacion de los campos de la base de datos marca, sku y se crea la tabla categoría

--- tienda/catalogo/models.py
+++ tienda/catalogo/models.py
@@ -1,12 +1,22 @@
+import random
+import string
 from django.db import models
 
 # Create your models here.
+class Categoria(models.Model):
+    nombre = models.CharField(max_length=50)
+    
+    def __str__(self):
+        return self.nombre
+
 class Producto(models.Model):
     # 'id' se crea automáticamente por Django como un AutoField (Clave Primaria)
     
     nombre = models.CharField(max_length=100)
     precio = models.IntegerField()  
     descripcion = models.TextField()
+    marca = models.CharField(max_length=50, null=True, blank=True)
+    sku = models.CharField(max_length=20, unique=True, null=True, blank=True)
     
     # En base de datos se guarda la ruta como texto
     imagen = models.CharField(max_length=200, default='imagengenerica.jpg')
@@ -14,8 +24,19 @@ class Producto(models.Model):
     stock = models.IntegerField(default=0)
     oferta = models.BooleanField(default=False)
     
-    # Recomendación extra: añade la categoría para poder filtrar
-    #categoria = models.CharField(max_length=50, null=True, blank=True)
+    # Relación con categoría
+    categoria = models.ForeignKey(Categoria, on_delete=models.SET_NULL, null=True, blank=True)
 
     def __str__(self):
         return self.nombre
+    
+    def save(self, *args, **kwargs):
+        # Si el producto no tiene SKU, lo generamos
+        if not self.sku:
+            while True:
+                nuevo_sku = "REP-" + ''.join(random.choices(string.ascii_uppercase + string.digits, k=6))
+                # Nos aseguramos de que sea único en la base de datos
+                if not Producto.objects.filter(sku=nuevo_sku).exists():
+                    self.sku = nuevo_sku
+                    break
+        super().save(*args, **kwargs)
